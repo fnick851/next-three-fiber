13a6a1d61633791636d671f6d6c9edde4693c4c2
Noah <hanlun.song@gmail.com>
2021-01-18 13:59:12 -0500

    update taliwind purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
+  purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
