96c129ffe78c2830ebe19cd4b829d8f41fe78143
Noah <hanlun.song@gmail.com>
2021-02-19 23:45:48 -0500

    refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,42 @@
 import Head from "next/head"
-import { Canvas } from "react-three-fiber"
+import { useEffect, useRef, useState } from "react"
+import { Canvas, MeshProps, useFrame } from "react-three-fiber"
+import { Mesh } from "three"
 import Layout from "../components/Layout"
-import Box from "../components/ClickableBox"
+
+function Box(props: MeshProps) {
+  const mesh = useRef<Mesh>()
+
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+
+  useEffect(() => {
+    const body = document.body
+    if (hovered) {
+      body.style.cursor = "pointer"
+    } else {
+      body.style.cursor = "default"
+    }
+  })
+
+  useFrame(() => {
+    if (mesh.current) mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+  })
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1, 1, 1] : [0.5, 0.5, 0.5]}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "steelblue" : "lightgreen"} />
+    </mesh>
+  )
+}
 
 export default function Home() {
   return (
