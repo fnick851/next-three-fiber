022c7520c7f85f0ab4af5f09d8e6dc2b313e571d
Noah <hanlun.song@gmail.com>
2021-02-22 11:59:04 -0500

    fix basic page title

--- pages/basic.tsx
+++ pages/basic.tsx
@@ -2,13 +2,12 @@ import Head from "next/head"
 import { Canvas } from "react-three-fiber"
 import Layout from "../components/Layout"
 import { OrbitControls } from "@react-three/drei"
-import LoadingScene from "../components/LoadingScene"
 
 export default function Basic() {
   return (
     <Layout>
       <Head>
-        <title>Camera</title>
+        <title>Basic</title>
       </Head>
 
       <Canvas className="bg-black">
